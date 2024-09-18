368244dd2058045dbfff27ce4810fca4c8ba64a3
hjdskhooba <bakisultanov@gmail.com>
2024-09-18 09:04:01 +0600

    metadatas is update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,45 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SEO ",
-  
+  title: "Unreal Kg",
+  description: "Маркетинговая компания Unreal Kg",
+    metadataBase: new URL("https://unreal.kg"),
+    keywords: [
+        "unreal",
+        "marketing",
+        "company",
+        "kg",
+        "unreal kg",
+        "анреал кг компания",
+        "анреал кг ",
+    ],
+    openGraph: {
+        type: 'website',
+        url: 'https://unreal.kg',
+        title: 'Unreal Kg',
+        description: 'Маркетинговая компания Unreal Kg',
+        siteName: 'Unreal Kg',
+        images: [
+            {
+                url: '/img/logo.jpg',
+                width: 1200,
+                height: 630,
+                alt: 'Unreal Kg Logo',
+            },
+        ],
+    },
+    creator: "Nevo Devs",
+    publisher: "Nevo Devs",
+    applicationName: "Unreal Kg",
+    authors: [{name :"Nevo Devs"}],
+    robots: {
+        index: true,
+        follow: true,
+    },
+    icons: {
+        icon: "/img/logo.jpg",
+        shortcut: "/img/logo.jpg",
+    },
 };
 
 export default function RootLayout({
@@ -15,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru">
        <head>
        <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" />
