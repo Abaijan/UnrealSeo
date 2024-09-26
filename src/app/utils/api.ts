78229364eaa833e6facf364edcb3135dd39c931e
hjdskhooba <bakisultanov@gmail.com>
2024-09-26 22:05:05 +0600

    Image component instead of img

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Ваш базовый URL
-    timeout: 1000, // Время ожидания ответа
     headers: { 'Content-Type': 'application/json' } // Заголовки по умолчанию
 });
 
